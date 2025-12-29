84f1b0ab7a82bb88bb213cf5dface4043d656fa0
Michal Gregor <michal@gregor.sk>
2025-12-29 15:35:32 +0100

    Automatic DummyTracker. - For convenience, when tracker is set to None (null) in a config, TrackedExperiment now automatically creates a DummyTracker instead of raising an error.

--- runexp/experiment.py
+++ runexp/experiment.py
@@ -1,5 +1,6 @@
 import abc
 from omegaconf import OmegaConf
+from .trackers import DummyTracker
 
 class BaseExperiment(abc.ABC):
     def __init__(self, config, config_built):
@@ -14,7 +15,15 @@ class TrackedExperiment(BaseExperiment):
     def __init__(self, config, config_built):
         super().__init__(config, config_built)
         self.tracker = self.config_built.run_args.tracker
-        self.tracker.setup(self.config)
+
+        if self.tracker is None:
+            self.tracker = DummyTracker(
+                self.config_built.run_args.project_name,
+                self.config_built.run_args.experiment_name,
+                self.config_built.run_args.description
+            )
+        else:
+            self.tracker.setup(self.config)
 
 class DummyExperiment(TrackedExperiment):
     def __call__(self):
